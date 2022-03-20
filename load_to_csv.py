3999a93cd339288fee72160a8f6fa811b5670f87
mxchirgin <maxchirgin@yandex.ru>
2022-03-20 16:42:18 +0300

    deleted unnecessary commands

--- load_to_csv.py
+++ load_to_csv.py
@@ -4,5 +4,5 @@ engine=sa.create_engine('postgresql+psycopg2://postgres:admin@localhost/postgres
 conn=engine.connect()
 
 f=pd.read_sql(con=conn, sql='select * from chirgin.result', index_col=None)
-print(f)
+
 f.to_excel('chirgin_result.xlsx',engine='openpyxl',index=None)
